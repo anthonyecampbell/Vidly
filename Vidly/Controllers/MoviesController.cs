c0396a3bbd5e46ad0753492344503fa87df73a31
Anthony Campbell <aecampbell12@outlook.com>
2018-01-08 23:28:45 -0600

    - removed hard coded movies list from movies controller

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -84,13 +84,5 @@ namespace Vidly.Controllers
             return Content(year + "/" + month);
         }
 
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" }
-            };
-        }
     }
 }
